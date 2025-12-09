7389c8a0bf2ba682ad6477b7286dd5a103e9aa1d
Tomasz Persa <tomasz.stanislaw.persa@gmail.com>
2025-12-09 21:50:32 +0100

    updated BookRepository Query

--- src/main/java/org/example/library/Repositories/BookRepository.java
+++ src/main/java/org/example/library/Repositories/BookRepository.java
@@ -10,25 +10,9 @@ import java.util.Optional;
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findBookByIsbn(String isbn);
 
-    // old method
-    @Query("""
-           SELECT b 
-           FROM Book b 
-           WHERE LOWER(b.title) LIKE :query 
-              or LOWER(CONCAT(b.author.firstName, ' ', b.author.lastName)) LIKE :query
-              OR LOWER(b.author.firstName) LIKE :query
-              OR LOWER(b.author.lastName) LIKE :query
-              or lower(b.category.name) LIKE :query
-              or lower(b.publisher.name) LIKE :query
-           """)
-    List<Book> search(String query);
-
-/*
     @Query("""
 SELECT b FROM Book b where b.category.id IN :categoryIds and b.isbn not in :excludedBookIsbns
 """)
     List<Book> findByCategoryIdInAndIdNotIn(List<Integer> categoryIds, List<String> excludedBookIsbns);
     }
-
- */
 }
