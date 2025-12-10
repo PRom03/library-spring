945b1654b1eb6d77e13f02b94626cdfbb1e742d2
PRom03 <romaniakp03@gmail.com>
2025-12-10 13:48:18 +0100

    feat added search integeration test

---
+++ src/test/java/org/example/library/SearchControllerTest.java
@@ -0,0 +1,42 @@
+package org.example.library;
+
+import org.example.library.Controllers.SearchController;
+import org.example.library.Repositories.AuthorRepository;
+import org.example.library.Repositories.BookRepository;
+import org.example.library.Repositories.CategoryRepository;
+import org.example.library.Repositories.PublisherRepository;
+import org.example.library.Services.BookService;
+import org.example.library.Services.SearchService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.mockito.Mockito.mock;
+
+@WebMvcTest(SearchController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class SearchControllerTest {
+    @MockitoBean
+    private SearchService searchService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testSearchRequest() throws Exception {
+        String query="test";
+        var result=this.mockMvc.perform(get("/api/search?q="+query)).andReturn();
+
+        assertThat(result.getResponse().getStatus()).isEqualTo(200);
+        verify(searchService).search(query);
+    }
+}
